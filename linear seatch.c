db59db98fa26d38c0876902aad0c2ff42f3f7db0
Himanshu Jaiswal <55348957+itshimanshu010@users.noreply.github.com>
2023-04-04 10:50:00 +0530

    Create linear seatch.c

---
+++ linear seatch.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int arr[10]={1,2,3,4,5,6,7,8,9,10};
+int i;
+int m;
+int find;
+
+printf("Enter your number :\n");
+scanf("%d",&m);
+
+for(i=0;i<10;i++)
+{
+    scanf("%d "&arr[i]);
+}
+
+for(i=0;i<10;i++)
+{
+    if(n=arr[i])
+    {
+        find++;
+    }
+}
+if(find>=1)
+{
+    printf("Number is found");
+}
+else{
+    printf("number is not found");
+}
+ return(0);
+}
